c8aaf7c274e4b259ae903c1e02e50967d51a5576
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-22 11:10:15 +0200

    image placeholder done

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,17 +3,20 @@
 const button = document.querySelector('.search');
 const input = document.querySelector('.input');
 const list = document.querySelector('.list');
+const imgDefault = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
 function search () {
+    list.innerHTML = '';
     const url = `http://api.tvmaze.com/search/shows?q=${input.value}`;
     fetch(url)
         .then (response => response.json())
         .then (data => {
             console.log(data);
             for (const result of data) {
-                list.innerHTML += `<li><img class="photo" src="${result.show.image.medium}"<br>${result.show.name}</li>`;
-                if (result.show.image.medium === null) {
-                    list.innerHTML += `<li><img class="photo" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"<br>${result.show.name}</li>`;
+                if (result.show.image === null) {
+                    list.innerHTML += `<li><img class="photo" src="${imgDefault}"<br>${result.show.name}</li>`;
+                } else {
+                    list.innerHTML += `<li><img class="photo" src="${result.show.image.medium}"<br>${result.show.name}</li>`;
                 }
             }
         });
