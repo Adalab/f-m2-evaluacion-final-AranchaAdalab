6a3869a8169de3cf531b1792d33aef07a1e2c647
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-22 10:04:28 +0200

    list the search

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('>> Ready :)');
+const button = document.querySelector('.search');
+const input = document.querySelector('.input');
+const list = document.querySelector('.list');
+
+function search () {
+    const url = `http://api.tvmaze.com/search/shows?q=${input.value}`;
+    fetch(url)
+        .then (response => response.json())
+        .then (data => {
+            console.log(data);
+            for (const result of data) {
+                list.innerHTML += `<li><img class="photo" src="${result.show.image.medium}"<br>${result.show.name}</li>`;
+                if (result.show.image.medium === null) {
+                    list.innerHTML += `<li><img class="photo" src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV"<br>${result.show.name}</li>`;
+                }
+            }
+        });
+}
+
+button.addEventListener('click', search);
+
