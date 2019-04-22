67aee8a6faacfe170169771b94db40c8fc62edd2
AranchaAdalab <aranzazunarvaez@gmail.com>
2019-04-22 13:40:57 +0200

    draw favourites list

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,34 +1,46 @@
+/* eslint-disable no-inner-declarations */
 'use strict';
 
 const button = document.querySelector('.search');
 const input = document.querySelector('.input');
 const list = document.querySelector('.list');
+const listFavourites = document.querySelector ('.myFavourites');
 const imgDefault = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
+
+
 function search () {
-    list.innerHTML = '';
-    const url = `http://api.tvmaze.com/search/shows?q=${input.value}`;
-    fetch(url)
-        .then (response => response.json())
-        .then (data => {
-            console.log(data);
-            for (const result of data) {
-                if (result.show.image === null) {
-                    list.innerHTML += `<li class="serie"><img class="photo" src="${imgDefault}"<br>${result.show.name}</li>`;
-                } else {
-                    list.innerHTML += `<li class="serie"><img class="photo" src="${result.show.image.medium}"<br>${result.show.name}</li>`;
-                }
+  list.innerHTML = '';
+  const url = `http://api.tvmaze.com/search/shows?q=${input.value}`;
+  fetch(url)
+    .then (response => response.json())
+    .then (data => {
+      console.log(data);
+
+      for (const result of data) {
+        if (result.show.image === null) {
+          list.innerHTML += `<li class="serie"><img class="photo" src="${imgDefault}"<br>${result.show.name}</li>`;
+        } else {
+          list.innerHTML += `<li class="serie"><img class="photo" src="${result.show.image.medium}"<br>${result.show.name}</li>`;
+        }
+        const everyShow = document.querySelectorAll('li');
+        let favourites = [];
+        for (const li of everyShow) {
+          function fav() {
+            li.classList.toggle('favourite');
+            if (li.classList.contains('favourite')) {
+              favourites.push({li});
+              listFavourites.innerHTML += `<li>${li.innerHTML}</li>`;
+              console.log(favourites);
             }
-            const li = document.querySelectorAll('li');
-            for (const item of li) {
-                function fav() {
-                    item.classList.add('favourite');
-                }
-                item.addEventListener('click', fav);
-            }
-        });
+          }
+          li.addEventListener('click', fav);
+        }
+      }
+    });
 }
 
 button.addEventListener('click', search);
+
 
 
